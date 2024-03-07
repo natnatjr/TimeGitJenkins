807773d4747b4c1e9ab995d5a5bbba7881e357c4
natnatjr <natnatjr@shernet.sheridancollege.com>
2024-03-07 13:53:46 -0500

    test 3

--- eclipse-workspace/Time/src/time/Time.java
+++ eclipse-workspace/Time/src/time/Time.java
@@ -23,7 +23,7 @@ public class Time
 {
 	public static void main(String[] args) 
 	{
-		int totalSeconds = getTotalSeconds("10:10:10");
+		int totalSeconds = getTotalSeconds("10:10:10:09");
 		System.out.println("**GH Total Seconds = "+totalSeconds);
 	}
 		
